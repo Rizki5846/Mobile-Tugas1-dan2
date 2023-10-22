b6d902286b43c958d1aa9c6a6b0618647324eb0c
Rizki5846 <134706927+Rizki5846@users.noreply.github.com>
2023-10-22 09:24:32 +0700

    Tugas2

---
+++ Tugas 1/string.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Membuat 3 variabel string
+  String string1 = "MUHAMAD";
+  String string2 = " ";
+  String string3 = "RIZKI";
+
+  // Menggabungkan ketiga string
+  String hasilGabungan = string1 + string2 + string3;
+
+  // Menampilkan hasilnya di layar
+  print(hasilGabungan);
+}
